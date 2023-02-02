6697328901c53879d21022c543593816d4fa316c
jarvisar <jarvisar@mail.uc.edu>
2023-02-01 20:17:48 -0500

    Combined bar charts into 1, added filtering

---
+++ js/barChart.js
@@ -0,0 +1,173 @@
+class BarChart {
+ 
+  constructor(_config, _data, _title, _callback, _color) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: _config.containerWidth || 500,
+      containerHeight: _config.containerHeight || 140,
+      margin: {top: 40, right: 50, bottom: 10, left: 50}
+    }
+	
+	this.callback = _callback;
+	this.color = _color;
+	this.title = _title;
+	this.data = _data; 
+	this.initVis();
+  }
+  
+   
+  initVis(){
+	let vis = this;
+	let dataArray = vis.data;
+	// Width and height as the inner dimensions of the chart area- as before
+    vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+    vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+
+    // Define 'svg' as a child-element (g) from the drawing area and include spaces
+    // Add <svg> element (drawing space)
+    vis.svg = d3.select(vis.config.parentElement)
+        .attr('width', vis.config.containerWidth)
+        .attr('height', vis.config.containerHeight)
+
+    vis.chart = vis.svg.append('g')
+        .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
+	
+	
+
+	vis.svg = d3.select(vis.config.parentElement)
+	  .attr('width', vis.config.containerWidth)
+	  .attr('height', vis.config.containerHeight)
+	  .classed('svg', true);
+
+	// Set the dimensions of the chart
+	const width = vis.config.containerWidth;
+	const height = vis.config.containerHeight;
+	const margin = 40;
+
+	// Set the scales
+	vis.xScale = d3.scaleBand()
+	.domain(dataArray.map(d => d.key))
+	.range([margin, width - margin])
+	.padding(0.1);
+
+	vis.yScale = d3.scaleLinear()
+	.domain([0, d3.max(dataArray, d => d.count)])
+	.range([height - margin, margin]);
+
+	vis.svg.append("text")
+	.attr("x", width/2)
+	.attr("y", 20)
+	.attr("text-anchor", "middle")
+	.style("font-family", "Roboto")
+	.style("font-size", "14px")
+	.text(vis.title);
+		// Add the y axis
+	vis.svg.append('g')
+		.attr('transform', `translate(${margin}, 0)`)
+		.call(d3.axisLeft(vis.yScale));
+	
+	vis.updateVis(dataArray);
+
+	// Add the x axis
+	if(vis.title == "Exoplanets by Discovery Method"){
+		vis.svg.append('g')
+        .attr('transform', `translate(0, ${vis.config.containerHeight - vis.config.margin.top})`)
+        .call(d3.axisBottom(vis.xScale))
+        .selectAll("text")
+        .style("text-anchor", "start")
+        .style("word-wrap", "break-word")
+        .style("font-family", "Roboto")
+        .style("color", "black")
+        .style("font-size", "9px")
+        .attr("dx", "0.5em")
+        .attr("dy", "-0.5em")
+        .attr("transform", "rotate(-90)")
+		.style("pointer-events", "none");
+	} else{
+		vis.svg.append('g')
+        .attr('transform', `translate(0, ${vis.config.containerHeight - vis.config.margin.top})`)
+        .call(d3.axisBottom(vis.xScale))
+        .selectAll("text")
+        .style("text-anchor", "middle")
+        .style("word-wrap", "break-word")
+        .style("font-family", "Roboto")
+        .style("color", "black")
+        .style("font-size", "9px")
+		.style("pointer-events", "none");
+	}
+  }
+
+  updateVis(){
+	let vis = this;
+	let dataArray = vis.data;
+    vis.clicked = {};
+
+	const width = vis.config.containerWidth;
+	const height = vis.config.containerHeight;
+	const margin = 40;
+
+    vis.rects = vis.svg.selectAll('rect')
+        .data(dataArray)
+        .join('rect')
+        .attr('x', d => vis.xScale(d.key))
+        .attr('y', d => vis.yScale(d.count))
+        .attr('width', vis.xScale.bandwidth())
+        .attr('height', d => vis.config.containerHeight - vis.config.margin.top - vis.yScale(d.count))
+        .style('fill', vis.color)
+        .attr('class', (d) => "bar-" + vis.title.replace(/\s+/g, '-').replace(/[/\\*]/g, "").replace(/\#/g, "").toLowerCase() + d.key.replace(/\s+/g, '-').replace(/[/\\*]/g, "").toLowerCase())
+
+    vis.rects.on('mouseover', (event, d) => {
+        let barClass = "bar-" + vis.title.replace(/\s+/g, '-').replace(/[/\\*]/g, "").replace(/\#/g, "").toLowerCase() + d.key.replace(/\s+/g, '-').replace(/[/\\*]/g, "").toLowerCase();
+        let brightness = vis.clicked[barClass] ? '80%' : '90%';
+        d3.select("." + barClass)
+            .style('filter', `brightness(${brightness})`);
+    })
+
+    vis.rects.on('mouseleave', (event, d) => {
+        let barClass = "bar-" + vis.title.replace(/\s+/g, '-').replace(/[/\\*]/g, "").replace(/\#/g, "").toLowerCase() + d.key.replace(/\s+/g, '-').replace(/[/\\*]/g, "").toLowerCase();
+        let brightness = vis.clicked[barClass] ? '80%' : '100%';
+        d3.select("." + barClass)
+            .style('filter', `brightness(${brightness})`);
+    });
+
+    vis.rects.on('click', (event, d) => {
+        let barClass = "bar-" + vis.title.replace(/\s+/g, '-').replace(/[/\\*]/g, "").replace(/\#/g, "").toLowerCase() + d.key.replace(/\s+/g, '-').replace(/[/\\*]/g, "").toLowerCase();
+		vis.clicked = {};
+        vis.clicked[barClass] = true;
+		console.log(d.key);
+		vis.callback(d.key);
+        vis.rects.style('filter', 'brightness(100%)');
+		d3.select("." + "bar-" + vis.title.replace(/\s+/g, '-').replace(/[/\\*]/g, "").replace(/\#/g, "").toLowerCase() + d.key.replace(/\s+/g, '-').replace(/[/\\*]/g, "").toLowerCase())
+		.style('filter', 'brightness(80%)');
+    });
+
+		// Add the reset button
+	vis.resetButton = vis.svg.append('rect')
+		.attr('x', margin)
+		.attr('y', height - 20)
+		.attr('width', 70)
+		.attr('height', 20)
+		.style('fill', 'lightgray')
+		.style('cursor', 'pointer');
+
+	vis.resetButton.on('click', (event, d) => {
+		vis.clicked = {};
+		vis.rects.style('filter', `brightness(100%)`);
+		vis.callback("reset");
+	});
+
+	vis.resetButton.append('title')
+		.text('Reset');
+
+	vis.resetButtonText = vis.svg.append('text')
+		.attr('x', margin + 35)
+		.attr('y', height - 5)
+		.style('text-anchor', 'middle')
+		.style('fill', 'white')
+		.style('cursor', 'pointer')
+		.style("pointer-events", "none")
+		.text('Reset');
+
+  }
+
+}
--- js/main.js
+++ js/main.js
@@ -5,41 +5,310 @@ d3.csv('data/exoplanets-1.csv')
   	console.log('Data loading complete. Work with dataset.');
     console.log(data);
 
+	// Discovery Method Bar Chart
+	let methodBar = new BarChart({
+		'parentElement': '#methodbar',
+		'containerHeight': 400,
+		'containerWidth': 300
+	}, getMethodCount(data), "Exoplanets by Discovery Method", (filterData) => {
+		let selectedFilter = [filterData];
+		console.log(filterData);
+		if (filterData != "reset"){
+			methodBar.data = getMethodCount(data.filter(d => selectedFilter.includes(d.discoverymethod)));
+			systemStarBar.data = getStarCount(data.filter(d => selectedFilter.includes(d.discoverymethod)));
+			systemPlanetBar.data = getPlanetCount(data.filter(d => selectedFilter.includes(d.discoverymethod)));
+			typeBar.data = getTypeCount(data.filter(d => selectedFilter.includes(d.discoverymethod)));
+			habitabilityBar.data = getHabitabilityCount(data.filter(d => selectedFilter.includes(d.discoverymethod)));
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		} else if (filterData == "reset") {
+			methodBar.data = getMethodCount(data);
+			console.log(methodBar.data);
+			systemStarBar.data = getStarCount(data);
+			systemPlanetBar.data = getPlanetCount(data);
+			typeBar.data = getTypeCount(data);
+			habitabilityBar.data = getHabitabilityCount(data);
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		}
+		
+	}, "#66d9ef");
 
-  	// Create an instance (for example in main.js)
-		let systemStarBar = new SystemStarBar({
-			'parentElement': '#starbar',
-			'containerHeight': 400,
-			'containerWidth': 300
-		}, data);
-		
-		let systemPlanetBar = new SystemPlanetBar({
-			'parentElement': '#planetbar',
-			'containerHeight': 400,
-			'containerWidth': 300
-		}, data);
-		
-		let typeBar = new TypeBar({
-			'parentElement': '#typebar',
-			'containerHeight': 400,
-			'containerWidth': 300
-		}, data);
-		
-		let methodBar = new MethodBar({
-			'parentElement': '#methodbar',
-			'containerHeight': 400,
-			'containerWidth': 300
-		}, data);
-		
 
-		let habitabilityBar = new HabitabilityBar({
-			'parentElement': '#habitablebar',
-			'containerHeight': 400,
-			'containerWidth': 300
-		}, data);
+  	// Planets in System Bar Chart
+	let systemStarBar = new BarChart({
+		'parentElement': '#starbar',
+		'containerHeight': 400,
+		'containerWidth': 300
+	}, getStarCount(data), "Exoplanets by # of Stars in their System", (filterData) => {
+		let selectedFilter = [filterData];
+		if (filterData != "reset"){
+			methodBar.data = getMethodCount(data.filter(d => selectedFilter.includes(d.sy_snum)));
+			systemStarBar.data = getStarCount(data.filter(d => selectedFilter.includes(d.sy_snum)));
+			systemPlanetBar.data = getPlanetCount(data.filter(d => selectedFilter.includes(d.sy_snum)));
+			typeBar.data = getTypeCount(data.filter(d => selectedFilter.includes(d.sy_snum)));
+			habitabilityBar.data = getHabitabilityCount(data.filter(d => selectedFilter.includes(d.sy_snum)));
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		} else if (filterData == "reset") {
+			methodBar.data = getMethodCount(data);
+			systemStarBar.data = getStarCount(data);
+			systemPlanetBar.data = getPlanetCount(data);
+			typeBar.data = getTypeCount(data);
+			habitabilityBar.data = getHabitabilityCount(data);
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		}
+		
+	}, '#0483e9');
+
+
+	// Planets in System Bar Chart
+	let systemPlanetBar = new BarChart({
+		'parentElement': '#planetbar',
+		'containerHeight': 400,
+		'containerWidth': 300
+	}, getPlanetCount(data), "Exoplanets by # of Planets in their System", (filterData) => {
+		let selectedFilter = [filterData];
+		if (filterData != "reset"){
+			methodBar.data = getMethodCount(data.filter(d => selectedFilter.includes(d.sy_pnum)));
+			systemStarBar.data = getStarCount(data.filter(d => selectedFilter.includes(d.sy_pnum)));
+			systemPlanetBar.data = getPlanetCount(data.filter(d => selectedFilter.includes(d.sy_pnum)));
+			typeBar.data = getTypeCount(data.filter(d => selectedFilter.includes(d.sy_pnum)));
+			habitabilityBar.data = getHabitabilityCount(data.filter(d => selectedFilter.includes(d.sy_pnum)));
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		} else if (filterData == "reset") {
+			methodBar.data = getMethodCount(data);
+			console.log(methodBar.data);
+			systemStarBar.data = getStarCount(data);
+			systemPlanetBar.data = getPlanetCount(data);
+			typeBar.data = getTypeCount(data);
+			habitabilityBar.data = getHabitabilityCount(data);
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		}
+		
+	}, '#104494');
+
+
+	// Star Type Bar	
+	let typeBar = new BarChart({
+		'parentElement': '#typebar',
+		'containerHeight': 400,
+		'containerWidth': 300
+	}, getTypeCount(data), "Exoplanets by Star Type", (filterData) => {
+		let selectedFilter = [filterData];
+		if (filterData != "reset" && filterData != "N/A"){
+			methodBar.data = getMethodCount(data.filter(d => selectedFilter.includes(d.st_spectype[0])));
+			systemStarBar.data = getStarCount(data.filter(d => selectedFilter.includes(d.st_spectype[0])));
+			systemPlanetBar.data = getPlanetCount(data.filter(d => selectedFilter.includes(d.st_spectype[0])));
+			typeBar.data = getTypeCount(data.filter(d => selectedFilter.includes(d.st_spectype[0])));
+			habitabilityBar.data = getHabitabilityCount(data.filter(d => selectedFilter.includes(d.st_spectype[0])));
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		} else if(filterData == "N/A"){
+			methodBar.data = getMethodCount(data.filter(d => d.st_spectype[0] != "A" && d.st_spectype[0] != "F" && d.st_spectype[0] != "G" && d.st_spectype[0] != "K" && d.st_spectype[0] != "M" ));
+			systemStarBar.data = getStarCount(data.filter(d => d.st_spectype[0] != "A" && d.st_spectype[0] != "F" && d.st_spectype[0] != "G" && d.st_spectype[0] != "K" && d.st_spectype[0] != "M" ));
+			systemPlanetBar.data = getPlanetCount(data.filter(d => d.st_spectype[0] != "A" && d.st_spectype[0] != "F" && d.st_spectype[0] != "G" && d.st_spectype[0] != "K" && d.st_spectype[0] != "M" ));
+			typeBar.data = getTypeCount(data.filter(d => d.st_spectype[0] != "A" && d.st_spectype[0] != "F" && d.st_spectype[0] != "G" && d.st_spectype[0] != "K" && d.st_spectype[0] != "M" ));
+			habitabilityBar.data = getHabitabilityCount(data.filter(d => d.st_spectype[0] != "A" && d.st_spectype[0] != "F" && d.st_spectype[0] != "G" && d.st_spectype[0] != "K" && d.st_spectype[0] != "M" ));
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		} else if (filterData == "reset"){
+			methodBar.data = getMethodCount(data);
+			console.log(methodBar.data);
+			systemStarBar.data = getStarCount(data);
+			systemPlanetBar.data = getPlanetCount(data);
+			typeBar.data = getTypeCount(data);
+			habitabilityBar.data = getHabitabilityCount(data);
+			methodBar.updateVis();
+			typeBar.updateVis();
+			systemStarBar.updateVis();
+			systemPlanetBar.updateVis();
+			habitabilityBar.updateVis();
+		}
+	}, '#1a0f35');
+
+	let habitabilityBar = new BarChart({
+		'parentElement': '#habitablebar',
+		'containerHeight': 400,
+		'containerWidth': 300
+	}, getHabitabilityCount(data), "Exoplanets by Habitability", (filterData) => { 
+		let selectedFilter = [filterData];
+		
+		methodBar.data = getMethodCount(data);
+		console.log(methodBar.data);
+		systemStarBar.data = getStarCount(data);
+		systemPlanetBar.data = getPlanetCount(data);
+		typeBar.data = getTypeCount(data);
+		habitabilityBar.data = getHabitabilityCount(data);
+		methodBar.updateVis();
+		typeBar.updateVis();
+		systemStarBar.updateVis();
+		systemPlanetBar.updateVis();
+		habitabilityBar.updateVis();
+	}, '#a1e9f7');
 
 })
 .catch(error => {
     console.error('Error loading the data');
 	console.error(error);
 });
+
+function getMethodCount(datar){
+	const counts = {};
+	datar.forEach(d => {
+		if (!counts[d.discoverymethod]) {
+		  counts[d.discoverymethod] = 1;
+		} else {
+		  counts[d.discoverymethod]++;
+		}
+	});
+	// Convert the counts object to an array of objects
+	dataArray = [];
+	for (let key in counts) {
+		dataArray.push({key: key, count: counts[key]});
+	}
+	return dataArray;
+}
+
+function getTypeCount(datar){
+	const counts = {'A': 0, 'F': 0, 'G': 0, 'K': 0, 'M': 0, 'N/A': 0 };
+	datar.forEach(d => {
+		if (['A', 'F', 'G', 'K', 'M', 'undefined'].includes(d.st_spectype[0])) {
+			counts[d.st_spectype[0]] = counts[d.st_spectype[0]] + 1 || 1;
+		} else {
+			counts['N/A'] = counts['N/A'] + 1 || 1;
+		}
+	});
+
+	// Convert the counts object to an array of objects
+	const dataArray = [];
+	for (let key in counts) {
+		dataArray.push({key: key, count: counts[key]});
+	}
+	return dataArray;
+}
+
+function getPlanetCount(datar) {
+	const counts = {};
+	datar.forEach(d => {
+		if (!counts[d.sy_pnum]) {
+		  counts[d.sy_pnum] = 1;
+		} else {
+		  counts[d.sy_pnum]++;
+		}
+	});
+
+	// Convert the counts object to an array of objects
+	const dataArray = [];
+	for (let key in counts) {
+		dataArray.push({key: key, count: counts[key]});
+	}
+	return dataArray;
+}
+
+function getStarCount(datar) {
+	const counts = {};
+	datar.forEach(d => {
+		if (!counts[d.sy_snum]) {
+		  counts[d.sy_snum] = 1;
+		} else {
+		  counts[d.sy_snum]++;
+		}
+	});
+
+	// Convert the counts object to an array of objects
+	const dataArray = [];
+	for (let key in counts) {
+		dataArray.push({key: key, count: counts[key]});
+	}
+	return dataArray;
+}
+
+function getHabitabilityCount(datar){
+	const counts = {'Habitable': 0, 'Too Cold': 0, 'Too Hot': 0};
+
+	datar.forEach(d => {
+	  switch (d.st_spectype[0]) {
+		case 'A':
+		  if (d.pl_orbsmax >= 8.5 && d.pl_orbsmax <= 12.5) {
+			counts['Habitable']++;
+		  } else if (d.pl_orbsmax < 8.5) {
+			counts['Too Cold']++;
+		  } else {
+			counts['Too Hot']++;
+		  }
+		  break;
+		case 'F':
+		  if (d.pl_orbsmax >= 1.5 && d.pl_orbsmax <= 2.2) {
+			counts['Habitable']++;
+		  } else if (d.pl_orbsmax < 1.5) {
+			counts['Too Cold']++;
+		  } else {
+			counts['Too Hot']++;
+		  }
+		  break;
+		case 'G':
+		  if (d.pl_orbsmax >= 0.95 && d.pl_orbsmax <= 1.4) {
+			counts['Habitable']++;
+		  } else if (d.pl_orbsmax < 0.95) {
+			counts['Too Cold']++;
+		  } else {
+			counts['Too Hot']++;
+		  }
+		  break;
+		case 'K':
+		  if (d.pl_orbsmax >= 0.38 && d.pl_orbsmax <= 0.56) {
+			counts['Habitable']++;
+		  } else if (d.pl_orbsmax < 0.38) {
+			counts['Too Cold']++;
+		  } else {
+			counts['Too Hot']++;
+		  }
+		  break;
+		case 'M':
+		  if (d.pl_orbsmax >= 0.08 && d.pl_orbsmax <= 0.12) {
+			counts['Habitable']++;
+		  } else if (d.pl_orbsmax < 0.08) {
+			counts['Too Cold']++;
+		  } else {
+			counts['Too Hot']++;
+		  }
+		  break;
+		default:
+		  break;
+	  }
+	});
+	// Convert the counts object to an array of objects
+	const dataArray = [];
+	for (let key in counts) {
+		dataArray.push({key: key, count: counts[key]});
+	}
+	return dataArray;
+}
